bd6d2294f293a8e68da2520eb667f757949b619d
Ritika Gakher <ritikagakher@Ritikas-MacBook-Pro.local>
2025-05-25 19:21:10 +0530

    support multiple and variable-length delimiters

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,11 +1,30 @@
 # lib/string_calculator.rb
-
 class StringCalculator
   def add(numbers)
     return 0 if numbers.empty?
+
     delimiters = [",", "\n"]
     #segregating delimiters and adding numbers
-    nums = numbers.split(/,|\n/).map(&:to_i)
+    
+    if numbers.start_with?("//")
+      delimiter_line, numbers = numbers.split("\n", 2)
+      custom_delimiter = delimiter_line[2..-1]
+
+      if custom_delimiter.start_with?("[") && custom_delimiter.end_with?("]")
+        delimiters = custom_delimiter.scan(/\[(.*?)\]/).flatten
+      else
+        delimiters = [custom_delimiter]
+      end
+    end
+
+    pattern = Regexp.union(delimiters)
+    nums = numbers.split(pattern).map(&:to_i)
+
+    negatives = nums.select { |n| n < 0 }
+    unless negatives.empty?
+      raise "negative numbers not allowed #{negatives.join(",")}"
+    end
+
     nums.sum
   end
 end
--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -6,22 +6,66 @@ RSpec.describe StringCalculator do
   let(:calculator) { StringCalculator.new }
 
   describe '#add' do
-    #empty string
+    # string with negative test case
+    context "with negative numbers" do
+      it "raises an error with a single negative number" do
+        expect { calculator.add("9,-4,3") }.to raise_error(RuntimeError, "negative numbers not allowed -4")
+      end
+
+      it "raises an error with multiple negative numbers" do
+        expect { calculator.add("-2,6,-7") }.to raise_error(RuntimeError, "negative numbers not allowed -2,-7")
+      end
+
+      it "raises an error with negative numbers and custom delimiter" do
+        expect { calculator.add("//;\n-1;2;-3") }.to raise_error(RuntimeError, "negative numbers not allowed -1,-3")
+      end
+
+      it "raises an error with negative numbers and multi-char delimiter" do
+        expect { calculator.add("//[***]\n-5***3***-2") }.to raise_error(RuntimeError, "negative numbers not allowed -5,-2")
+      end
+    end
+
+    # empty string
     it 'returns 0 for empty string' do
       expect(calculator.add('')).to eq(0)
     end
-    #single input
+
+    # single input
     it 'returns the number for single input' do
       expect(calculator.add('4')).to eq(4)
     end
-    #comma-separated
+
+    # comma-separated
     it 'returns the sum for comma-separated numbers' do
       expect(calculator.add('1,2')).to eq(3)
     end
-    #string with delimiters
+
+    # string with delimiters
     it "handles new lines between numbers as delimiters" do
       expect(calculator.add("1\n2,3")).to eq(6)
       expect(calculator.add("4\n5\n6")).to eq(15)
+    end
+
+    # string with custom delimiters
+    context "with custom delimiters" do
+      it "supports a single-character custom delimiter" do
+        expect(calculator.add("//;\n4;5")).to eq(9)
+        expect(calculator.add("//|\n2|7|1")).to eq(10)
+      end
+
+      it "supports custom delimiters of any length" do
+        expect(calculator.add("//[***]\n6***2***3")).to eq(11)
+        expect(calculator.add("//[abc]\n3abc3abc1")).to eq(7)
+      end
+
+      it "supports multiple custom delimiters" do
+        expect(calculator.add("//[*][%]\n2*1%4")).to eq(7)
+        expect(calculator.add("//[;;][@@]\n1;;3@@8")).to eq(12)
+      end
+
+      it "supports multiple custom delimiters of different lengths" do
+        expect(calculator.add("//[***][%%]\n6***2%%6")).to eq(14)
+      end
     end
   end
 end
