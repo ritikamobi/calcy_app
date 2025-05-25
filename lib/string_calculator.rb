52b234bec0e15c12c3123cb98ba25ad7a2397fbd
Ritika Gakher <ritikagakher@Ritikas-MacBook-Pro.local>
2025-05-25 18:57:07 +0530

    added logic for delimiters

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,6 +1,11 @@
+# lib/string_calculator.rb
+
 class StringCalculator
   def add(numbers)
     return 0 if numbers.empty?
-    numbers.split(/,|\n/).map(&:to_i).sum
+    delimiters = [",", "\n"]
+    #segregating delimiters and adding numbers
+    nums = numbers.split(/,|\n/).map(&:to_i)
+    nums.sum
   end
 end
--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -6,16 +6,22 @@ RSpec.describe StringCalculator do
   let(:calculator) { StringCalculator.new }
 
   describe '#add' do
+    #empty string
     it 'returns 0 for empty string' do
       expect(calculator.add('')).to eq(0)
     end
-
+    #single input
     it 'returns the number for single input' do
       expect(calculator.add('4')).to eq(4)
     end
-
+    #comma-separated
     it 'returns the sum for comma-separated numbers' do
       expect(calculator.add('1,2')).to eq(3)
+    end
+    #string with delimiters
+    it "handles new lines between numbers as delimiters" do
+      expect(calculator.add("1\n2,3")).to eq(6)
+      expect(calculator.add("4\n5\n6")).to eq(15)
     end
   end
 end
